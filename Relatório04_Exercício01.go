98fed5a1afdda8431a023d74052ce38268f02bbd
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2025-09-05 19:12:05 -0300

    Relatório04_Exercício01.go

---
+++ Relatório04_Exercício01.go
@@ -0,0 +1,22 @@
+package main
+
+import "fmt"
+
+func classificar_numero(num int) string {
+	if num > 0 {
+		return "Positivo"
+	} else if num < 0 {
+		return "Negativo"
+	} else {
+		return "Zero"
+	}
+}
+
+func main() {
+	fmt.Println("Digite um número para classificá-lo: ")
+	
+	var input int
+	fmt.Scan(&input)
+
+	fmt.Println("O número ", input, " é ", classificar_numero(input))
+}
