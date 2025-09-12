327335c6b86de2fb494e1b955bbede248260313b
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2025-09-12 12:37:25 -0300

    Relatório05_Exercício02.cpp

---
+++ Relatório05_Exercício02.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+float somaArray(float arr[], int tamanho) {
+  float soma = 0;
+  for (int i = 0; i < tamanho; i++) {
+    soma += arr[i];
+  }
+  return soma;
+}
+
+int main() {
+  cout << "Quantos numeros voce quer somar? \n";
+  int tam;
+  cin >> tam;
+
+  cout << "Digite os numeros a ser somados: \n";
+  float arr[tam];
+
+  for (int i = 0; i < tam; i++) {
+    cin >> arr[i];
+  }
+
+  cout << "A soma dos numeros e: " << somaArray(arr, tam);
+}
