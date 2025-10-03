788c28d4d5ed64a315228af6858998757d851c61
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2025-10-03 18:56:35 -0300

    Relatório07_Exercício01.cs

---
+++ Relatório07_Exercício01.cs
@@ -0,0 +1,26 @@
+using System;
+
+public class MembroDaSociedade {
+  private string nome;
+  private string raca;
+  private string funcao;
+
+  public MembroDaSociedade(string nome, string raca, string funcao){
+    this.nome = nome;
+    this.raca = raca;
+    this.funcao = funcao;
+  }
+
+  public void Descrever(){
+    Console.WriteLine($" Nome: {nome};\n Raça: {raca};\n Função: {funcao}.\n");
+  }
+}
+
+class Program {
+  public static void Main (string[] args) {
+    MembroDaSociedade Aragorn = new MembroDaSociedade("Aragorn", "Humano", "Guardião");
+    MembroDaSociedade Legolas = new MembroDaSociedade("Legolas", "Elfo", "Arqueiro");
+    Aragorn.Descrever();
+    Legolas.Descrever();
+  }
+}
