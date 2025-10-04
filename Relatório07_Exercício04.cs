9d1326615b8ae851717c7e257c1090b21cfa949f
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2025-10-04 14:28:25 -0300

    Relatório07_Exercício04.cs

---
+++ Relatório07_Exercício04.cs
@@ -0,0 +1,45 @@
+using System;
+using System.Collections.Generic;
+
+public class MonstroSombrio {
+  public string nome;
+
+  public MonstroSombrio(string nome){
+    this.nome = nome;
+  }
+
+  public virtual void Mover(){
+    Console.WriteLine($"O {nome}(monstro) esta se movendo");
+  }
+}
+
+public class Zumbi : MonstroSombrio {
+  public Zumbi(string nome) : base(nome){}
+
+  public override void Mover(){
+    Console.WriteLine($"O {nome}(zumbi) esta se movendo de forma arrastada");
+  }
+}
+
+public class Espectro : MonstroSombrio {
+  public Espectro(string nome) : base(nome){}
+
+  public override void Mover(){
+    Console.WriteLine($"O {nome}(espectro) esta se movendo no ar");
+  }
+}
+
+class Program {
+  public static void Main (string[] args) {
+    MonstroSombrio[] monstros = new MonstroSombrio[4];
+
+    monstros[0] = new Zumbi("Rentt Faina");
+    monstros[1] = new Zumbi("Momonga");
+    monstros[2] = new Espectro("SukkalGir");
+    monstros[3] = new Espectro("Velha Turbo");
+
+    foreach(MonstroSombrio monstro in monstros){
+      monstro.Mover();
+    }
+  }
+}
