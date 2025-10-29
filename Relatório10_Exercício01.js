6917ac61bbb718773f6389571e6749a28c45bbca
Marcelo Rodrigues Rosa Paschoal <marcrodprv@gmail.com>
2025-10-29 09:55:32 -0300

    Relatório10.Exercício01.js

---
+++ Relatório10_Exercício01.js
@@ -0,0 +1,69 @@
+class Pokemon {
+    #vida;
+    constructor(nome, tipo, vidaInicial){
+        this.nome = nome;
+        this.tipo = tipo;
+        this.#vida = vidaInicial;
+    }
+
+    getVida(){
+        return this.#vida;
+    }
+
+    receberDano(dano){
+        this.#vida -= dano;
+        if (this.#vida < 0) this.#vida = 0;
+    }
+
+    atacar(alvo){
+        const dano = 5;
+        alvo.receberDano(dano);
+        console.log(`${this.nome} utilizou Tackle em ${alvo.nome} e causou ${dano} de dano.`);
+    }
+}
+
+class PokemonFogo extends Pokemon {
+    constructor(nome, vidaInicial, bonus){
+        super(nome, 'Fogo', vidaInicial);
+        this.bonusAtaque = bonus;
+    }
+
+    atacar(alvo){
+        const dano = 10 + this.bonusAtaque;
+        alvo.receberDano(dano);
+        console.log(`${this.nome} utilizou Bola de Fogo em ${alvo.nome} e causou ${dano} de dano.`);
+    }
+}
+
+class PokemonAgua extends Pokemon {
+    #curaBase;
+    constructor(nome, vidaInicial, curaBase){
+        super(nome, 'Água', vidaInicial);
+        this.#curaBase = curaBase;
+    }
+
+    atacar(alvo){
+        const dano = 8;
+        alvo.receberDano(dano);
+        console.log(`${this.nome} utilizou Jato D'água em ${alvo.nome} e causou ${dano} de dano.`);
+        console.log(`${this.nome} recuperou ${this.#curaBase} de vida.`);
+    }
+}
+
+//--------------Main------------------
+Eevee = new Pokemon('Eevee', 'Normal', 20)
+Blastoise = new PokemonAgua('Blastoise', 120, 5)
+Charizard = new PokemonFogo('Charizard', 120, 10)
+
+pokemons = [Eevee, Blastoise, Charizard];
+
+pokemons.forEach(pokemon => {
+    pokemon.atacar(Eevee);
+    pokemon.atacar(Blastoise);
+    pokemon.atacar(Charizard);
+});
+
+console.log("\n=======STATUS PÓS BATALHA=======");
+pokemons.forEach(p => {
+    console.log(`${p.nome} (${p.tipo}) - Vida: ${p.getVida()}`);
+});
